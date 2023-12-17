41874ef196ed4a0b35e495ec1431728f924bdb54
Daniela Soares <danielasmelo232@gmail.com>
2023-12-17 19:02:39 -0300

    iniciando projeto

---
+++ src/views/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css'
+import Menu from "../components/layout/Menu";
+import Content from "../components/layout/Content";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const App = props => (
+    <div className="App">
+        <Router>
+            <Menu />
+            <Content />
+        </Router>
+    </div>
+)
+export default App
+{/*const App = props => (
+    <div className="App">
+        <Menu />
+        <Content />
+        <BrowserRouter />
+    </div>
+)*/}
---
+++ src/views/examples/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = props => (
+    <div className="About">
+        <h1>Sobre</h1>
+        <h2>Este sistema foi criado apenas para treinar navegação.</h2>
+        <ul>
+            <li>Navegação simples</li>
+            <li>Navegação com parâmetros</li>
+            <li>Navegação com parâmetro Exact e Notfound</li>
+        </ul>
+    </div>
+)
+export default About
